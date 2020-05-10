e24cb8436200d2160676f4c05809f06a0c825798
vijay0405 <uvijaysimha@gmail.com>
2020-05-10 23:47:05 +0530

    play a random sound on keydown

--- app.js
+++ app.js
@@ -2,24 +2,53 @@ const insert = document.getElementById('insert');
 const body = document.getElementById('main-body');
 
 function generateRandomColor(num) {
-	var arr = [];
-	for (var i = 0; i < num; i++) {
-		arr.push(randomColors());
-	}
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push(randomColors());
+    }
 
-	return arr;
+    return arr;
 }
 
-function randomColors(){
-	var r = Math.floor(Math.random() * 256);	
-	var g = Math.floor(Math.random() * 256);	
-	var b = Math.floor(Math.random() * 256);
-	return "rgb("+r+", "+g+", "+b+")";	
+var sounds = [
+    'bubbles',
+    'corona',
+    'flash-2',
+    'moon',
+    'piston-2',
+    'prism-2',
+    'squiggle',
+    'timer',
+    'wipe',
+    'clay',
+    'dotted-spiral',
+    'flash-3',
+    'pinwheel',
+    'piston-3',
+    'prism-3',
+    'strike',
+    'ufo',
+    'zig-zag',
+    'confetti',
+    'flash-1',
+    'glimmer',
+    'piston-1',
+    'prism-1',
+    'splits',
+    'suspension',
+    'veil'
+];
+
+function randomColors() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 window.addEventListener('keydown', (e) => {
-	console.log(e);
-	insert.innerHTML = `
+    console.log(e);
+    insert.innerHTML = `
 		<div class="key">
 			${e.key === ' ' ? 'Space' : e.key}
 			<small>event.key</small>
@@ -35,4 +64,6 @@ window.addEventListener('keydown', (e) => {
     `
     let colors = generateRandomColor(50);
     body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    let sound = sounds[Math.floor(Math.random() * sounds.length)]
+    new Howl({ src: ['sounds/' + sound + '.mp3'] }).play();
 });
